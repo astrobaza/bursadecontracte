0894a7d2179209cf192fb6044afc2ce16a3571a2
astronautulroz <51267976+astronautulroz@users.noreply.github.com>
2020-03-31 01:27:50 +0300

    db

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'icontrac_wp281' );
+define( 'DB_NAME', 'bc' );
 
 /** MySQL database username */
-define( 'DB_USER', 'icontrac_wp281' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '88p]RK2CS(' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
